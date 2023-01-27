d249bf487c62cdfade4b9f945c5519279f0af14f
riccardofano <rcr.fano@gmail.com>
2023-01-27 14:46:44 +0100

    feat: handle create feedback post requests

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 use std::net::SocketAddr;
 
-use axum::{routing::get, Router};
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Form, Json, Router,
+};
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/feedback/new", post(create_request));
+
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
     tracing::info!("listening on {addr}");
@@ -19,4 +28,36 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello world"
+}
+
+#[derive(Serialize, Deserialize)]
+struct CreateRequest {
+    title: String,
+    category: String,
+    description: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Request {
+    id: u32,
+    title: String,
+    category: String,
+    upvotes: u32,
+    status: String,
+    description: String,
+    comments: Option<String>,
+}
+
+async fn create_request(Form(payload): Form<CreateRequest>) -> impl IntoResponse {
+    let new_request = Request {
+        id: 1,
+        title: payload.title,
+        category: payload.category,
+        upvotes: 0,
+        status: String::from("suggestion"),
+        description: payload.description,
+        comments: None,
+    };
+
+    (StatusCode::CREATED, Json(new_request))
 }
