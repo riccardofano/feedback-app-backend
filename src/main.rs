2e2f50d0c155137038d94ca110129a65d6548747
riccardofano <rcr.fano@gmail.com>
2023-01-27 14:21:50 +0100

    feat: initialize server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(root));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    tracing::info!("listening on {addr}");
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("could not start server");
+}
+
+async fn root() -> &'static str {
+    "Hello world"
 }
