4f496a6993a430b904fe6f2d13673aa344efe4a6
riccardofano <rcr.fano@gmail.com>
2023-01-29 19:30:33 +0100

    feat: setup CORS

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,14 @@ mod feedback;
 use std::sync::{Arc, RwLock};
 
 use axum::{
+    http::Method,
     routing::{get, patch, post},
     Router,
 };
 
 use feedback::create_request;
 use sync_wrapper::SyncWrapper;
+use tower_http::cors::{Any, CorsLayer};
 
 use crate::{
     appstate::AppState,
@@ -23,6 +25,9 @@ type SharedState = Arc<RwLock<AppState>>;
 #[shuttle_service::main]
 async fn axum() -> shuttle_service::ShuttleAxum {
     let state = Arc::new(RwLock::new(AppState::seeded()));
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST, Method::PATCH])
+        .allow_origin(Any);
 
     let app = Router::new()
         .route("/", get(root))
@@ -33,7 +38,8 @@ async fn axum() -> shuttle_service::ShuttleAxum {
         .route("/feedback/:id/upvote", post(upvote_request))
         .route("/feedback/:id/comment", post(create_comment))
         .route("/comments/:id/reply", post(create_reply))
-        .with_state(state);
+        .with_state(state)
+        .layer(cors);
 
     let sync_wrapper = SyncWrapper::new(app);
 
