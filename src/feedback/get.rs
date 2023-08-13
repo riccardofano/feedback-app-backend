02162e001f1291ee32799ba85fb241b2bdccea32
Riccardo Fano <rcr.fano@gmail.com>
2023-08-13 11:55:17 +0200

    feat: retrieve the base comments with the request

--- src/feedback/get.rs
+++ src/feedback/get.rs
@@ -6,16 +6,31 @@ use axum::{
     Json,
 };
 
-use crate::schema::Request;
+use crate::schema::{Comment, Request};
 use crate::Context;
 
 #[debug_handler]
 pub async fn get_request(Path(id): Path<i32>, State(context): State<Context>) -> impl IntoResponse {
-    let row = sqlx::query_as!(Request, "SELECT * FROM Request WHERE id = $1", id)
-        .fetch_optional(&context.pool)
-        .await
-        .ok()
-        .flatten();
+    let row = sqlx::query_as!(
+        Request,
+        r#"SELECT r.*,
+        ARRAY_AGG((
+            c.id,
+            c.id_request,
+            c.id_parent,
+            c.owner,
+            c.content
+        )) as "comments: Vec<Comment>"
+        FROM Request r
+        LEFT JOIN Comment c
+            ON r.id = c.id_request AND c.id_parent IS NULL
+        WHERE r.id = $1
+        GROUP BY r.id"#,
+        id
+    )
+    .fetch_optional(&context.pool)
+    .await
+    .expect("Could not fetch request");
 
     match row {
         Some(request) => (StatusCode::OK, Json(Some(request))),
--- src/schema.rs
+++ src/schema.rs
@@ -1,7 +1,7 @@
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 use sqlx::FromRow;
 
-#[derive(FromRow, Serialize, Deserialize)]
+#[derive(Debug, FromRow, Serialize)]
 pub struct Request {
     pub id: i32,
     pub title: String,
@@ -10,4 +10,50 @@ pub struct Request {
     pub upvoted: bool,
     pub status: String,
     pub description: String,
+    pub comments: Option<Vec<Comment>>,
+}
+
+#[derive(Debug, FromRow, Serialize)]
+pub struct Comment {
+    pub id: i32,
+    pub id_request: i32,
+    pub id_parent: Option<i32>,
+    pub owner: String,
+    pub content: String,
+}
+
+// NOTE: I had to manually implement Decode instead of deriving sqlx::Type
+// because there's a bug with rustc where it can't properly resolve the lifetype
+// of a type when there's also the same type but wrapped in Option<T>
+// (i32 and Option<i32> in this case)
+// https://github.com/launchbadge/sqlx/issues/1031
+impl<'r> ::sqlx::decode::Decode<'r, ::sqlx::Postgres> for Comment {
+    fn decode(
+        value: ::sqlx::postgres::PgValueRef<'r>,
+    ) -> ::std::result::Result<
+        Self,
+        ::std::boxed::Box<
+            dyn ::std::error::Error + 'static + ::std::marker::Send + ::std::marker::Sync,
+        >,
+    > {
+        let mut decoder = ::sqlx::postgres::types::PgRecordDecoder::new(value)?;
+        let id = decoder.try_decode::<i32>()?;
+        let id_request = decoder.try_decode::<i32>()?;
+        let id_parent = decoder.try_decode::<Option<i32>>()?;
+        let owner = decoder.try_decode::<String>()?;
+        let content = decoder.try_decode::<String>()?;
+        ::std::result::Result::Ok(Comment {
+            id,
+            id_request,
+            id_parent,
+            owner,
+            content,
+        })
+    }
+}
+
+impl ::sqlx::Type<::sqlx::Postgres> for Comment {
+    fn type_info() -> ::sqlx::postgres::PgTypeInfo {
+        ::sqlx::postgres::PgTypeInfo::with_name("Comment")
+    }
 }
